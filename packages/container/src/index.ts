a51278f4bb09d9ac6ba5489abfb012dad11dcc15
chok <maxime.picaud@harchibald.com>
2026-09-17 00:28:51 +0200

    feat(container): export ContainerError

--- packages/container/src/index.ts
+++ packages/container/src/index.ts
@@ -1,10 +1,12 @@
 import { ScopeContainer } from './ScopeContainer.js';
+import { ContainerError } from './ContainerError.js';
 import type { Container } from './Container.js';
 import type { Constructor } from './registration/Constructor.js';
 import type { Lifetime } from './registration/Lifetime.js';
 import type { RegisterOptions } from './registration/RegisterOptions.js';
 
 export type { Container, Constructor, RegisterOptions, Lifetime };
+export { ContainerError };
 
 export function createContainer(): Container {
   return new ScopeContainer();
